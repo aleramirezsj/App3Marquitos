8f609aeb88736698e0e7c1799a9124ba02a55587
aleramirezsj <aleramirezsj@gmail.com>
2022-08-26 15:36:40 -0300

    Creación del helper

---
+++ App3/App3/Core/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace App3.Core
+{
+    public class Helper
+    {
+        public static HttpClient ObtenerClienteHttp()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("x-apikey", "2c677ccf2cb62a940092248e128001983dab0");
+            return client;
+        }
+    }
+}

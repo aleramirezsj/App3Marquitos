a7b060ffcebb7c7a3024780709549a63aa856ad8
aleramirezsj <aleramirezsj@gmail.com>
2022-10-14 14:52:07 -0300

    modelo actividad agregado

---
+++ App3/App3/Models/Actividad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App3.Models
+{
+    public class Actividad
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Horarios { get; set; }
+        public decimal Costo { get; set; }
+    }
+}
